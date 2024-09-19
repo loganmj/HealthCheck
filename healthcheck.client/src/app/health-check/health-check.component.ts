f6e6166e004b066bdb4d31a24118d5e7cdd81646
Michael <michaellogan7860@gmail.com>
2024-09-19 13:40:43 -0500

    Add health-check.component.ts. Added/updated some npm packages to allow for auto-formatting the project.

--- healthcheck.client/src/app/health-check/health-check.component.ts
+++ healthcheck.client/src/app/health-check/health-check.component.ts
@@ -1,10 +1,74 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment';
+import { tap, catchError, of } from 'rxjs';
 
+/**
+ * A component used to perform HealthChecks against the server.
+ */ 
 @Component({
   selector: 'app-health-check',
   templateUrl: './health-check.component.html',
   styleUrl: './health-check.component.css'
 })
-export class HealthCheckComponent {
+export class HealthCheckComponent implements OnInit {
 
+  // #region Properties
+
+  /**
+   * Custom http result property.
+   */
+  public result?: Result;
+
+  // #endregion
+
+  // #region Constructors
+
+  /**
+   * Constructor
+   */
+  constructor(private http: HttpClient) { }
+
+  // #endregion
+
+  // #region Private Methods
+
+  /**
+   * Lifetime management method.
+   * Called on initialization of the component.
+   */
+  ngOnInit(): void {
+    this.http.get<Result>(`${environment.baseUrl}api/health`).pipe(
+      tap(result => this.result = result),
+      catchError(error => {
+        console.error(error);
+        return of(undefined);
+      })
+    ).subscribe();
+  }
+
+  // #endregion
 }
+
+// #region Private Interfaces
+
+/**
+* Describes a single health check result.
+*/
+interface Check {
+  name: string;
+  responseTime: number;
+  status: string;
+  description: string;
+}
+
+/**
+ *  Describes a summarized result of all checks.
+ */
+interface Result {
+  checks: Check[];
+  totalStatus: string;
+  totalResponseTime: number;
+}
+
+// #endregion
