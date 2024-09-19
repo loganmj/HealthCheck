1f1246b0008376a6db0edde190bc6111d0e9c768
Michael <michaellogan7860@gmail.com>
2024-09-19 12:32:30 -0500

    Configured server to use custom health check definitions. Added health-check component to client project.

---
+++ healthcheck.client/src/app/health-check/health-check.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-health-check',
+  templateUrl: './health-check.component.html',
+  styleUrl: './health-check.component.css'
+})
+export class HealthCheckComponent {
+
+}
